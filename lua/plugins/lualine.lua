06ab9a567b15db9bcdfe5b77c548e4ddba79bdf1
Marco Servalli <mservalli@gmail.com>
2023-11-04 10:59:17 +0100

    feat(plugins): lualine install basic

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,61 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local icons = require("../bootstrap/icons")
+
+    local function gitdiff()
+      local gitsigns = vim.b.gitsigns_status_dict
+      if gitsigns then
+        return {
+          added = gitsigns.added,
+          modified = gitsigns.changed,
+          removed = gitsigns.removed,
+        }
+      end
+    end
+
+    local sections = {
+      lualine_a = { "mode" },
+      lualine_b = {
+        {
+          "b:gitsigns_head",
+          icon = "%#SLGitIcon#" .. icons.git.branch .. "%*" .. "%#SLBranchName#",
+          color = { gui = "bold" },
+        },
+      },
+      lualine_c = {
+        {
+          "diff",
+          source = gitdiff(),
+          symbols = {
+            added = icons.git.linesAdded .. " ",
+            modified = icons.git.linesChanged .. " ",
+            removed = icons.git.linesRemoved .. " ",
+          },
+          padding = { left = 2, right = 1 },
+        },
+      },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    }
+
+    local config = {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        icons_enabled = true,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = { "alpha" },
+      },
+      sections = sections,
+      inactive_sections = sections,
+      tabline = {},
+      extensions = {},
+    }
+
+    require("lualine").setup(config)
+  end,
+}
