98358348e6786bedf4b13fd8c7fd9a09fde98280
Marco Servalli <mservalli@gmail.com>
2023-11-04 10:11:10 +0100

    feat(options): improve custom +leader to <space>

--- lua/bootstrap/options.lua
+++ lua/bootstrap/options.lua
@@ -1,31 +1,36 @@
+vim.g.mapleader = " "
+
 -- backup settings
-vim.o.backup = true
+vim.opt.backup = true
 vim.opt.swapfile = true
 vim.opt.writebackup = false
 
 local backupDir = vim.fn.expand("$HOME/.backup")
-vim.o.backupdir = backupDir .. "/.backup" -- cidectory for backup file
-vim.o.directory = backupDir .. "/.backup" -- directory for temp file
+vim.opt.backupdir = backupDir .. "/.backup" -- cidectory for backup file
+vim.opt.directory = backupDir .. "/.backup" -- directory for temp file
 
 -- vistual options
-vim.o.foldenable = false
-vim.o.foldmethod = "indent"
-vim.o.mouse = "n"
-vim.o.numberwidth = 4
-vim.o.relativenumber = true
-vim.o.smarttab = true
-vim.o.so = 10 -- line at bottom of cursor line
-vim.o.softtabstop = 4
-vim.o.splitbelow = true
-vim.o.splitright = true
-vim.o.tabstop = 4
-vim.o.wrap = false
+vim.opt.fillchars = vim.opt.fillchars + "eob: "
+vim.opt.foldenable = false
+vim.opt.foldmethod = "indent"
+vim.opt.mouse = "n"
+vim.opt.numberwidth = 4
+vim.opt.relativenumber = true
+vim.opt.smarttab = true
+vim.opt.so = 10 -- line at bottom of cursor line
+vim.opt.softtabstop = 4
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 4
+vim.opt.wrap = false
 vim.opt.cursorline = true
+vim.opt.expandtab = true
 vim.opt.laststatus = 3
 vim.opt.ruler = false
 vim.opt.scrolloff = 0
 vim.opt.shiftwidth = 2
 vim.opt.showcmd = false
+vim.opt.showmode = false
 vim.opt.showtabline = 1
 vim.opt.sidescrolloff = 8
 vim.opt.signcolumn = "yes"
