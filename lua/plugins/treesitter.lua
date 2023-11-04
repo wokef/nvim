88fa87ffa9f559990885104184e91013a8acb56e
Marco Servalli <mservalli@gmail.com>
2023-11-04 17:04:12 +0100

    feat(plugins): treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = {
+          enable = true,
+        },
+        indent = { enable = true },
+        autotag = {
+          enable = true,
+        },
+      })
+    end,
+  },
+}
