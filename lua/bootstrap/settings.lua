f7b9879a46c59c42c2ec1956fbb85317a3f3cdd4
Marco Servalli <mservalli@gmail.com>
2023-11-04 10:12:28 +0100

    feat(colorscheme): install poimandres +stylua

--- lua/bootstrap/settings.lua
+++ lua/bootstrap/settings.lua
@@ -1,3 +1,3 @@
 return {
-	colorscheme = "tokionight",
+  colorscheme = "poimandres",
 }
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,7 @@
+return {
+  "olivercederborg/poimandres.nvim",
+  priority = 100,
+  config = function()
+    vim.cmd("colorscheme poimandres")
+  end,
+}
