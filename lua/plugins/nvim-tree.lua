a2fd0c7429414f9409e57c530b37f5a1a2292874
Marco Servalli <mservalli@gmail.com>
2023-11-04 10:13:06 +0100

    feat(plugins): nvim-tree + base config

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,125 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    require("nvim-tree").setup({
+      sort_by = "name",
+      sync_root_with_cwd = true,
+      on_attach = "default",
+      hijack_cursor = false,
+      renderer = {
+        add_trailing = false,
+        group_empty = false,
+        highlight_git = true,
+        full_name = false,
+        highlight_opened_files = "none",
+        root_folder_label = ":t",
+        indent_width = 2,
+        indent_markers = {
+          enable = false,
+        },
+        icons = {
+          git_placement = "before",
+          padding = " ",
+        },
+        symlink_destination = true,
+      },
+      view = {
+        width = 50,
+        side = "left",
+        number = false,
+        relativenumber = false,
+        signcolumn = "yes",
+      },
+      git = {
+        enable = true,
+        ignore = false,
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+        timeout = 200,
+      },
+      filters = {
+        dotfiles = false,
+        git_clean = false,
+        no_buffer = false,
+        custom = { "node_modules", "\\.cache" },
+        exclude = {},
+      },
+      hijack_directories = {
+        enable = false,
+        auto_open = true,
+      },
+      update_focused_file = {
+        enable = true,
+        debounce_delay = 15,
+        update_root = true,
+        ignore_list = {},
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = false,
+        show_on_open_dirs = true,
+        debounce_delay = 50,
+        severity = {
+          min = vim.diagnostic.severity.HINT,
+          max = vim.diagnostic.severity.ERROR,
+        },
+      },
+      filesystem_watchers = {
+        enable = true,
+        debounce_delay = 50,
+        ignore_dirs = {},
+      },
+      actions = {
+        use_system_clipboard = true,
+        change_dir = {
+          enable = true,
+          global = false,
+          restrict_above_cwd = false,
+        },
+        expand_all = {
+          max_folder_discovery = 300,
+          exclude = {},
+        },
+        file_popup = {
+          open_win_config = {
+            col = 1,
+            row = 1,
+            relative = "cursor",
+            border = "shadow",
+            style = "minimal",
+          },
+        },
+        open_file = {
+          quit_on_open = false,
+          resize_window = false,
+          window_picker = {
+            enable = true,
+            picker = "default",
+            chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+            exclude = {
+              filetype = { "notify", "lazy", "qf", "diff", "fugitive", "fugitiveblame" },
+              buftype = { "nofile", "terminal", "help" },
+            },
+          },
+        },
+        remove_file = {
+          close_window = true,
+        },
+      },
+      trash = {
+        cmd = "trash",
+        require_confirm = true,
+      },
+      live_filter = {
+        prefix = "[FILTER]: ",
+        always_show_folders = true,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle files explorer" })
+  end,
+}
