d833165fc788b0a7dcc62675eacbba3af71293f6
Marco Servalli <mservalli@gmail.com>
2023-11-05 12:28:28 +0100

    feat(plugins): nvim-treesitter,comment + ts_commentstring

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,6 +4,7 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
     dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
       "nvim-treesitter/nvim-treesitter-textobjects",
       "windwp/nvim-ts-autotag",
     },
