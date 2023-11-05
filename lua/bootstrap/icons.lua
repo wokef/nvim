564e607e7df1918e69d77a04247f7c0eb6efc381
Marco Servalli <mservalli@gmail.com>
2023-11-05 11:19:59 +0100

    feat(Plugins): lspconfig + nvim-cmp zero

--- lua/bootstrap/icons.lua
+++ lua/bootstrap/icons.lua
@@ -11,4 +11,10 @@ return {
     linesChanged = "",
     linesRemoved = "",
   },
+  lsp = {
+    Error = " ",
+    Warn = " ",
+    Hint = "󰠠 ",
+    Info = " ",
+  },
 }
---
+++ lua/plugins/lsp/lspconfig.lua
@@ -0,0 +1,53 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+  },
+  config = function()
+    local icons = require("../bootstrap.icons")
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+    local on_attach = function(_, bufnr)
+      local k = vim.keymap.set
+
+      local opts = function(desc)
+        return {
+          noremap = true,
+          silent = true,
+          buffer = bufnr,
+          desc = desc,
+        }
+      end
+
+      k("n", "gr", "<cmd>Telescope lsp_references<CR>", opts("Show LSP references"))
+      k("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+      k("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts("Show LSP definitions"))
+      k("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts("Show LSP implementations"))
+      k("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts("Show LSP type definitions"))
+      k({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts("See available code actions"))
+      k("n", "<leader>rn", vim.lsp.buf.rename, opts("Smart rename"))
+      k("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts("Show buffer diagnostics"))
+      k("n", "<leader>d", vim.diagnostic.open_float, opts("Show line diagnostics"))
+      k("n", "[d", vim.diagnostic.goto_prev, opts("Go to previous diagnostic"))
+      k("n", "]d", vim.diagnostic.goto_next, opts("Go to next diagnostic"))
+      k("n", "K", vim.lsp.buf.hover, opts("Show documentation for what is under cursor"))
+      k("n", "<leader>rs", ":LspRestart<CR>", opts("Restart LSP"))
+    end
+
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    local signs = icons.lsp
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+
+    lspconfig["phpactor"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+  end,
+}
