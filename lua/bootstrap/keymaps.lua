0ade68ef5c28ce5808ce56086db2b822a5e4f885
Marco Servalli <mservalli@gmail.com>
2023-11-05 16:24:15 +0100

    feat(keymaps): Lazygit toggle on <leader>gg

--- lua/bootstrap/keymaps.lua
+++ lua/bootstrap/keymaps.lua
@@ -41,7 +41,25 @@ k("n", "<leader>sr", "<cmd>Telescope oldfiles<CR>", { desc = "Search recent file
 k("n", "<leader>sb", "<cmd>Telescope buffers<CR>", { desc = "Search in buffers" })
 
 -- git
--- k("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Lazygit" })
+k("n", "<leader>gg", function()
+  local Terminal = require("toggleterm.terminal").Terminal
+  local lazygit = Terminal:new({
+    cmd = "lazygit",
+    hidden = true,
+    direction = "float",
+    float_opts = {
+      border = "none",
+      width = 100000,
+      height = 100000,
+    },
+    on_open = function(_)
+      vim.cmd("startinsert!")
+    end,
+    on_close = function(_) end,
+    count = 99,
+  })
+  lazygit:toggle()
+end, { desc = "Lazygit" })
 k("n", "<leader>gf", "<cmd>G<cr>", { desc = "Fugitive" })
 k("n", "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", { desc = "Next Hunk" })
 k("n", "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", { desc = "Prev Hunk" })
