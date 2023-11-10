04f3fa86ae861945eab06c6028d5b3cf30485f29
Marco Servalli <mservalli@gmail.com>
2023-11-10 11:20:49 +0100

    feat(lualine): lsp section with lsp list
    @TODO linters/formatters

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -54,6 +54,39 @@ return {
             hint = icons.lsp.Hint,
           },
         },
+        {
+          function()
+            local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+            if #buf_clients == 0 then
+              return "LSP Inactive"
+            end
+
+            -- local buf_ft = vim.bo.filetype
+            local buf_client_names = {}
+
+            for _, client in pairs(buf_clients) do
+              if client.name ~= "null-ls" and client.name ~= "copilot" then
+                table.insert(buf_client_names, client.name)
+              end
+            end
+
+            -- -- add formatter
+            -- local formatters = require("lvim.lsp.null-ls.formatters")
+            -- local supported_formatters = formatters.list_registered(buf_ft)
+            -- vim.list_extend(buf_client_names, supported_formatters)
+
+            -- -- add linter
+            -- local linters = require("lvim.lsp.null-ls.linters")
+            -- local supported_linters = linters.list_registered(buf_ft)
+            -- vim.list_extend(buf_client_names, supported_linters)
+
+            local unique_client_names = table.concat(buf_client_names, ", ")
+            local language_servers = string.format("[%s]", unique_client_names)
+
+            return language_servers
+          end,
+          color = { gui = "bold" },
+        },
         "filetype",
       },
       lualine_y = { "progress" },
