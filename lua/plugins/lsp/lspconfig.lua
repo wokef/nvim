e65c29380d52c16e8ea483760b23d81b74b03f2d
Marco Servalli <mservalli@gmail.com>
2023-11-23 12:59:31 +0100

    feat(lspconfig): Smart rename: <leader>ln -> <leader>lr

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -28,7 +28,7 @@ return {
       k("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts("Show LSP implementations"))
       k("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts("Show LSP type definitions"))
       k({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts("See available code actions"))
-      k("n", "<leader>ln", vim.lsp.buf.rename, opts("Smart rename"))
+      k("n", "<leader>lr", vim.lsp.buf.rename, opts("Smart rename"))
       k("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts("Show buffer diagnostics"))
       k("n", "<leader>d", vim.diagnostic.open_float, opts("Show line diagnostics"))
       k("n", "[d", vim.diagnostic.goto_prev, opts("Go to previous diagnostic"))
