315871235be39f5e84b4843ef9b7d26d7810293b
Marco Servalli <mservalli@gmail.com>
2023-12-01 09:57:59 +0100

    feat(noice): customize messages

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,7 +1,22 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    messages = {
+      view = false,
+      view_error = false,
+      view_warn = false,
+      view_history = false,
+      view_search = false,
+    },
+    lsp = {
+      progress = { enabled = false },
+      hover = { enabled = false },
+      signature = { enabled = false },
+      message = { enabled = false },
+      documentation = { enabled = false },
+    },
+  },
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
