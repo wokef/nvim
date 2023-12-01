7e0ca34a1f8512fd3a165bc9537ea3e04f8d7fff
Marco Servalli <mservalli@gmail.com>
2023-12-01 09:58:32 +0100

    feat(alpha-nvim): disable footer

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -24,36 +24,36 @@ return {
       dashboard.button("q", icons.close .. " Quit", "<cmd>qa<CR>"),
     }
 
-    local linux = os.getenv("WSL_DISTRO_NAME") .. [[ ]] .. os.getenv("HOSTTYPE")
-    local wslVersion = "N.A.    "
-    local winVersion = "N.A.          "
-    local kernelVersion = "N.A.     "
-    local wsl = io.popen("wsl.exe --version", "r")
-    local user = os.getenv("USER") .. [[@]] .. os.getenv("NAME")
-
-    if wsl ~= nil then
-      local versions = tostring(wsl:read("*a"))
-      wsl:close()
-      versions = versions:gsub("[^0-9.\n]", "")
-
-      local versionsTable = {}
-      for w in string.gmatch(versions, "[^\n]+") do
-        table.insert(versionsTable, w)
-      end
-
-      wslVersion = "WSL 2" .. versionsTable[1]:sub(versionsTable[1]:find(".") + 1)
-      kernelVersion = "Kernel " .. versionsTable[2]
-      winVersion = "Win 11 Pro " .. versionsTable[7]
-    end
-
-    dashboard.section.footer.opts.hl = "AlphaFooter"
-    dashboard.section.footer.val = {
-      [[ 👨 ┊ ]] .. user .. [[                                ]],
-      [[ 🪟 ┊ ]] .. winVersion,
-      [[ 🪅 ┊ ]] .. wslVersion,
-      [[ 🐧 ┊ ]] .. linux,
-      [[ 🧩 ┊ ]] .. kernelVersion,
-    }
+    -- local linux = os.getenv("WSL_DISTRO_NAME") .. [[ ]] .. os.getenv("HOSTTYPE")
+    -- local wslVersion = "N.A.    "
+    -- local winVersion = "N.A.          "
+    -- local kernelVersion = "N.A.     "
+    -- local wsl = io.popen("wsl.exe --version", "r")
+    -- local user = os.getenv("USER") .. [[@]] .. os.getenv("NAME")
+    --
+    -- if wsl ~= nil then
+    --   local versions = tostring(wsl:read("*a"))
+    --   wsl:close()
+    --   versions = versions:gsub("[^0-9.\n]", "")
+    --
+    --   local versionsTable = {}
+    --   for w in string.gmatch(versions, "[^\n]+") do
+    --     table.insert(versionsTable, w)
+    --   end
+    --
+    --   wslVersion = "WSL 2" .. versionsTable[1]:sub(versionsTable[1]:find(".") + 1)
+    --   kernelVersion = "Kernel " .. versionsTable[2]
+    --   winVersion = "Win 11 Pro " .. versionsTable[7]
+    -- end
+    --
+    -- dashboard.section.footer.opts.hl = "AlphaFooter"
+    -- dashboard.section.footer.val = {
+    --   [[ 👨 ┊ ]] .. user .. [[                                ]],
+    --   [[ 🪟 ┊ ]] .. winVersion,
+    --   [[ 🪅 ┊ ]] .. wslVersion,
+    --   [[ 🐧 ┊ ]] .. linux,
+    --   [[ 🧩 ┊ ]] .. kernelVersion,
+    -- }
 
     require("alpha").setup(dashboard.opts)
   end,
