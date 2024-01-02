25e90eb0bccacc4b569dd8c998fe32bc2bcea545
Marco Servalli <mservalli@gmail.com>
2024-01-02 12:31:53 +0100

    feat(colorscheme): move back to tokionight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-  "gbprod/nord.nvim",
+  "folke/tokyonight.nvim",
   priority = 100,
   config = function()
-    require("nord").setup({ transparent = true })
-    vim.cmd.colorscheme("nord")
+    vim.cmd.colorscheme("tokyonight")
 
     local c = require("bootstrap.colors")
 
@@ -13,5 +12,11 @@ return {
     vim.api.nvim_set_hl(0, "BufferCurrentSign", { fg = c.fuksia })
     vim.api.nvim_set_hl(0, "BufferLineIndicatorSelected", { fg = c.fuksia })
     vim.api.nvim_set_hl(0, "WinSeparator", { fg = c.cyan_light })
+
+    vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = c.cyan_native })
+    vim.api.nvim_set_hl(0, "GitSignsChange", { fg = c.blue_light })
+    vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = c.fuksia })
+
+    vim.api.nvim_set_hl(0, "NoiceMini", { fg = c.cyan_light, bg = c.black_light, italic = true })
   end,
 }
