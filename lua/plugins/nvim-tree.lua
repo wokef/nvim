a93bd3d5797db91eb36734936714f58c79fca868
Marco Servalli <mservalli@gmail.com>
2025-02-04 10:26:29 +0100

    feat(nvim-tree): show folder node_modules

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -47,7 +47,7 @@ return {
         dotfiles = false,
         git_clean = false,
         no_buffer = false,
-        custom = { "node_modules", "\\.cache" },
+        custom = { "\\.cache" },
         exclude = {},
       },
       hijack_directories = {
