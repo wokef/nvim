b28985254fd0b7b2b759d2fea4795cc69940da87
Marco Servalli <mservalli@gmail.com>
2025-05-31 10:40:53 +0200

    feat(Copilot): gpt-4o, custom keymap for accept all <A-l>

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,12 @@
 return {
   "github/copilot.vim",
+  config = function()
+    vim.g.copilot_settings = { selectedCompletionModel = 'gpt-4o-copilot' }
+
+    vim.api.nvim_set_keymap("i", "<A-l>", 'copilot#Accept("<CR>")', {
+      expr = true,
+      silent = true,
+      noremap = true,
+    })
+  end
 }
