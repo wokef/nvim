26ceb567175c9155408a2e37a93f8c76c3242057
Marco Servalli <mservalli@gmail.com>
2025-06-21 17:25:50 +0200

    feat: move win32yank autocommand to local.lua

--- lua/bootstrap/autocommands.lua
+++ lua/bootstrap/autocommands.lua
@@ -17,27 +17,3 @@ vim.api.nvim_create_user_command("LazyGit", function()
 end, {})
 
 vim.api.nvim_command([[set clipboard+=unnamedplus]])
-vim.g.clipboard = {
-  name = "win32yank-wsl",
-  copy = {
-    ["+"] = "/mnt/c/win32yank.exe -i --crlf",
-    ["*"] = "/mnt/c/win32yank.exe -i --crlf",
-  },
-  paste = {
-    ["+"] = "/mnt/c/win32yank.exe -o --lf",
-    ["*"] = "/mnt/c/win32yank.exe -o --lf",
-  },
-  cache_enabled = false,
-}
--- vim.g.clipboard = {
---   name = "myClipboard",
---   copy = {
---     ["+"] = "clip.exe",
---     ["*"] = "clip.exe",
---   },
---   paste = {
---     ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---     ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---   },
---   cache_enabled = false,
--- }
