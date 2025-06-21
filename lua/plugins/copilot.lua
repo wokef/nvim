e1e761161303a4da32e8a1e0a93e9f061554571f
Marco Servalli <mservalli@gmail.com>
2025-06-21 17:19:01 +0200

    feat(Copilot): new keymaps

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,12 @@
 return {
   "github/copilot.vim",
   config = function()
+    local options = { expr = true, silent = true, noremap = true }
+
     vim.g.copilot_settings = { selectedCompletionModel = 'gpt-4o-copilot' }
 
-    vim.api.nvim_set_keymap("i", "<A-l>", 'copilot#Accept("<CR>")', {
-      expr = true,
-      silent = true,
-      noremap = true,
-    })
+    vim.api.nvim_set_keymap("i", "<A-w>", 'copilot#AcceptWord()', options)
+    vim.api.nvim_set_keymap("i", "<A-l>", 'copilot#AcceptLine()', options)
+    vim.api.nvim_set_keymap("i", "<A-a>", 'copilot#Accept("<CR>")', options)
   end
 }
