2390aa35fc925b98ec059a6403db5ab4723e33e7
Marco Servalli <mservalli@gmail.com>
2025-08-26 11:32:41 +0200

    feat(Copilot): keymap <leader>C toggle prompt panel

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -8,5 +8,6 @@ return {
     vim.api.nvim_set_keymap("i", "<A-w>", 'copilot#AcceptWord()', options)
     vim.api.nvim_set_keymap("i", "<A-l>", 'copilot#AcceptLine()', options)
     vim.api.nvim_set_keymap("i", "<A-a>", 'copilot#Accept("<CR>")', options)
+    vim.keymap.set("n", "<leader>C", ":Copilot panel<CR>", { desc = "Apri Copilot Panel" })
   end
 }
