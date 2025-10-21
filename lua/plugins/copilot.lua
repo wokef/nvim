0458149aa3ec4c60d65ced7cd8fd49f764d92d70
Marco Servalli <mservalli@gmail.com>
2025-10-21 17:19:34 +0200

    feat(Copilot): Use default model

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,11 +3,11 @@ return {
   config = function()
     local options = { expr = true, silent = true, noremap = true }
 
-    vim.g.copilot_settings = { selectedCompletionModel = 'gpt-4o-copilot' }
+    -- vim.g.copilot_settings = { selectedCompletionModel = 'gpt-4o-copilot' }
 
-    vim.api.nvim_set_keymap("i", "<A-w>", 'copilot#AcceptWord()', options)
-    vim.api.nvim_set_keymap("i", "<A-l>", 'copilot#AcceptLine()', options)
+    vim.api.nvim_set_keymap("i", "<A-w>", "copilot#AcceptWord()", options)
+    vim.api.nvim_set_keymap("i", "<A-l>", "copilot#AcceptLine()", options)
     vim.api.nvim_set_keymap("i", "<A-a>", 'copilot#Accept("<CR>")', options)
     vim.keymap.set("n", "<leader>C", ":Copilot panel<CR>", { desc = "Apri Copilot Panel" })
-  end
+  end,
 }
