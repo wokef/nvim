f1f15b500f2a9a251b392c5274df8ca83a5fdc42
Marco Servalli <mservalli@gmail.com>
2026-02-26 11:19:06 +0100

    feat(Lualine): add progress plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -100,7 +100,10 @@ return {
           end,
         },
       },
-      lualine_z = { "location" },
+      lualine_z = {
+        { "progress", padding = { left = 1, right = 0 } },
+        { "location", padding = { left = 0, right = 1 } },
+      },
     }
 
     table.insert(sections.lualine_b, {
