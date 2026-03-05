f453911da806d03abbb9e70993014d209d1931e4
Marco Servalli <mservalli@gmail.com>
2026-03-05 15:23:16 +0100

    feat(deprecation): use vim.hl instead vim.highlight

--- lua/bootstrap/autocommands.lua
+++ lua/bootstrap/autocommands.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = { "*" },
   callback = function()
-    vim.highlight.on_yank({ higroup = "Search", timeout = 100 })
+    vim.hl.on_yank({ higroup = "Search", timeout = 100 })
   end,
 })
 
