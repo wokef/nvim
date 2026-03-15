fed50d56f25af4911469f982bf929deaaae85f27
Marco Servalli <mservalli@gmail.com>
2026-03-15 11:10:20 +0100

    feat(Lazy): remove tokyonight from settings, use global colorscheme instead

--- lua/bootstrap/lazy.lua
+++ lua/bootstrap/lazy.lua
@@ -1,35 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local settings = require("../bootstrap/settings")
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ import = "plugins" },
-	{ import = "plugins.lsp" },
+  { import = "plugins" },
+  { import = "plugins.lsp" },
 }, {
-	install = {
-		colorscheme = { settings.colorscheme },
-	},
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	ui = {
-		border = "rounded",
-	},
-	change_detection = {
-		enabled = true,
-		notify = false,
-	},
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  ui = {
+    border = "rounded",
+  },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
 })
