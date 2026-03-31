ef43f8f428d69194c8a5d7fbb0189660c6e540db
Marco Servalli <mservalli@gmail.com>
2026-03-31 09:53:47 +0200

    feat(colorscheme): command to quick change theme with restet current

--- lua/bootstrap/autocommands.lua
+++ lua/bootstrap/autocommands.lua
@@ -24,4 +24,37 @@ vim.api.nvim_create_user_command("ClearLogs", function()
   require("bootstrap.common").clear_logs()
 end, {})
 
+vim.api.nvim_create_user_command("CS", function()
+  require("telescope.pickers")
+    .new({
+      layout_strategy = "center",
+      layout_config = {
+        center = {
+          width = 0.2,
+        },
+      },
+    }, {
+      prompt_title = "Choose colorscheme",
+      finder = require("telescope.finders").new_table({
+        results = vim.fn.getcompletion("", "color"),
+      }),
+      sorter = require("telescope.sorters").get_generic_fuzzy_sorter(),
+      attach_mappings = function()
+        local actions = require("telescope.actions")
+
+        actions.select_default:replace(function()
+          local selection = require("telescope.actions.state").get_selected_entry()
+          if selection then
+            vim.cmd("highlight clear")
+            vim.cmd("syntax reset")
+
+            vim.cmd.colorscheme(selection[1])
+          end
+        end)
+        return true
+      end,
+    })
+    :find()
+end, {})
+
 vim.api.nvim_command([[set clipboard+=unnamedplus]])
