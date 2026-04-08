6d03328e8c24e4cd2bcc3af75a0cf6b379cd3644
Marco Servalli <mservalli@gmail.com>
2026-04-08 10:07:46 +0200

    feat(colors): restore some customs

--- lua/colorschemes/customizations.lua
+++ lua/colorschemes/customizations.lua
@@ -5,18 +5,18 @@ function M.apply()
 
   vim.api.nvim_set_hl(0, "Normal", { fg = c.fg, bg = c.bg })
   vim.api.nvim_set_hl(0, "NormalNC", { fg = c.fg, bg = c.bg })
-  -- vim.api.nvim_set_hl(0, "CursorLine", { bg = c.highlight })
-  -- vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = c.highlight })
+  vim.api.nvim_set_hl(0, "CursorLine", { bg = c.highlight })
+  vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = c.highlight })
 
   vim.api.nvim_set_hl(0, "CursorLineNr", { fg = c.cyan_light })
-  -- vim.api.nvim_set_hl(0, "ColorColumn", { bg = c.grey_dark })
+  vim.api.nvim_set_hl(0, "ColorColumn", { bg = c.grey_dark })
   vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = c.cyan_light })
   vim.api.nvim_set_hl(0, "GitSignsChange", { fg = c.blue })
   vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = c.fuksia })
   vim.api.nvim_set_hl(0, "IndentBlanklineChar", { fg = c.cyan_light, bg = c.cyan_light })
   vim.api.nvim_set_hl(0, "NoiceMini", { fg = c.cyan_light, bg = "", italic = true })
   vim.api.nvim_set_hl(0, "WinSeparator", { fg = c.cyan_light })
-  -- vim.api.nvim_set_hl(0, "Search", { fg = c.white, bg = c.red_dark })
+  vim.api.nvim_set_hl(0, "Search", { fg = c.white, bg = c.red_dark })
   vim.api.nvim_set_hl(0, "DashboardTitle", { fg = c.fuksia })
 end
 
