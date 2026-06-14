b270f3a70c82b517ca6a78caa2a339a54d3050ee
Marco Servalli <mservalli@gmail.com>
2026-06-14 13:20:09 +0200

    feat(colorscheme): add minimal customization

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,8 +6,16 @@ return {
     vim.cmd("syntax reset")
 
     vim.cmd.colorscheme("tokyonight-storm")
+
+    local c = require("bootstrap.colors")
+    local fn = vim.api.nvim_set_hl
+
+    fn(0, "CursorLineNr", { fg = c.cyan_light })
+    fn(0, "GitSignsAdd", { fg = c.cyan_light })
+    fn(0, "GitSignsChange", { fg = c.blue })
+    fn(0, "GitSignsDelete", { fg = c.fuksia })
+    fn(0, "WinSeparator", { fg = c.cyan_light })
+    fn(0, "Search", { fg = c.white, bg = c.fuksia })
   end,
-  dependencies = {
-    "folke/tokyonight.nvim",
-  },
+  dependencies = {},
 }
