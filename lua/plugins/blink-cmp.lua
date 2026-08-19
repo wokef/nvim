fcb5a9c1550873dee6fdacb680e3488a85089599
Marco Servalli <mservalli@gmail.com>
2026-08-19 10:26:26 +0200

    feat(plugin): blink.cmp, enable PgUp and PgDn navigation

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -14,8 +14,21 @@ return {
 
       ["<Up>"] = { "select_prev", "fallback" },
       ["<Down>"] = { "select_next", "fallback" },
-      ["<C-p>"] = { "select_prev", "fallback" },
-      ["<C-n>"] = { "select_next", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<PageUp>"] = {
+        function(cmp)
+          return cmp.select_prev({ count = 8 })
+        end,
+        "fallback",
+      },
+
+      ["<PageDown>"] = {
+        function(cmp)
+          return cmp.select_next({ count = 8 })
+        end,
+        "fallback",
+      },
 
       ["<C-b>"] = { "scroll_documentation_up", "fallback" },
       ["<C-f>"] = { "scroll_documentation_down", "fallback" },
